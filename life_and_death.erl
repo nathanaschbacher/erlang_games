c35ca6ad08516f5d9f84baf430adf84e77a6c6d9
Nathan Aschbacher <nathan.aschbacher@gmail.com>
2015-02-25 17:23:55 -0800

    Blargh!

---
+++ life_and_death.erl
@@ -0,0 +1,36 @@
+%%
+ % - Create an Earth!
+ % - Create some Inhabitants!
+ % - Give them a life to live!
+ % - Make them sociable!
+ %    - if they meet a zombie... they die.
+ %    - if they get old... they die.
+ % - Turn them into zombies when they die!
+ % - If/when the world is overrun, nuke it from orbit and try again.
+%%
+-module(life_and_death).
+-export([init/0,earth/1]).
+
+init() ->
+  Earth = spawn_link(?MODULE, earth, [{}]),
+  ok.
+
+earth(Inhabitants) ->
+  receive
+    Something ->
+      io:format("~p", [something]);
+    _ ->
+      io:format("Will anyone ever hear me?")
+  end,
+  earth(Inhabitants).
+
+human() ->
+  howdy.
+
+zombie() ->
+  blargh.
+
+biological_clock() ->
+  ok.
+  
+
